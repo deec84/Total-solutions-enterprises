dd65ecce772d9abafa402d257797f804933c5638
deec84 <TOTALSOLUTIONSENTERPRISES@GMAIL.COM>
2026-08-04 20:29:55 -0400

    fix(android): align JVM build targets

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -16,6 +16,15 @@ android {
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
